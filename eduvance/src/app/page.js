18fda6943fcefbf5afb4de25b78f034c9090f6f2
Rao <103417697+Aby-ss@users.noreply.github.com>
2025-04-18 19:13:12 +0400

    Added fonts + materials for webpages

--- eduvance/src/app/page.js
+++ eduvance/src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-
+import Image from 'next/image';
 import { useState, useEffect } from 'react';
 
 export default function Home() {
@@ -8,11 +8,17 @@ export default function Home() {
       {/* Gradient Box in Background */}
       <div className="absolute w-[95vw] h-[85vh] bg-gradient-to-b from-[#679CFF] to-[#0C60FB] rounded-2xl shadow-xl z-0" />
 
-      <h1 className="poppins-semibold text-5xl lg:text-5xl tracking-tight text-center z-10 relative text-white">
+      <h1 className="font-grand-local text-6xl lg:text-6xl tracking-tight text-center z-10 relative text-white">
         Advancing with
         <br />
         ducation
       </h1>
+
+      <img
+        src="/Logo 1.png"
+        alt="Eduvance Logo"
+        className="w-48 h-auto z-10"
+      />
     </main>
   );
 }
