c0a4500423a8d4e89491346e7ae2eae46cbd1e26
Rao <103417697+Aby-ss@users.noreply.github.com>
2025-04-24 08:33:41 +0400

    Added all subject pages for resources

---
+++ eduvance/src/app/sub_links/physics/page.js
@@ -0,0 +1,12 @@
+"use client";
+import Image from 'next/image';
+import Link from "next/link";
+import { useState, useEffect } from 'react';
+
+export default function Resources() {
+  return (
+          <main className="flex w-full h-screen">
+                    <img src="/Physics_Banner.png" alt="Physics" className="w-full h-auto"/>
+          </main>
+  );
+}
