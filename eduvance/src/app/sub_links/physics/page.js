631eaf8f47a52a4b60f618c9ab67cc677825174a
Rao <103417697+Aby-ss@users.noreply.github.com>
2025-04-27 10:59:22 +0400

    Added other subject resources page

--- eduvance/src/app/sub_links/physics/page.js
+++ eduvance/src/app/sub_links/physics/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from "next/link";
 import { useState, useEffect } from 'react';
 
-export default function Resources() {
+export default function Physics() {
 
   const [selected, setSelected] = useState('option1');
 
