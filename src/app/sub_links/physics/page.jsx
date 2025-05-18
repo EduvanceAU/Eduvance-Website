a6c6e49a0e23ed8b533835f451e7ca89c38b1b5b
Rao <103417697+Aby-ss@users.noreply.github.com>
2025-05-18 20:11:00 +0400

    Added base code for individual past paper pages

--- src/app/sub_links/physics/page.jsx
+++ src/app/sub_links/physics/page.jsx
@@ -72,7 +72,7 @@ export default function Physics() {
                 </Link>
 
                 <Link
-                  href="/page3"
+                  href="/sub_links/physics/pastpapers"
                   className="hover-tint w-full sm:w-[calc(50%-8px)] md:w-[calc(33.33%-16px)] h-40 rounded-xl font-[550] tracking-[-0.5px] border-[1.5px] border-[#153064] flex items-end justify-start pl-4 pb-4 text-[#153064] hover:bg-[#BAD1FD] transition-all duration-300 cursor-pointer"
                   style={{ backgroundImage: "url('/Papers Background.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}
                 >
