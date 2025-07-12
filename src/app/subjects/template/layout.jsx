fa7ebc2d040768976ff45e97c7faf16473c66da1
Anonymously Anonymous <99310000+anonymouslyanonymous1@users.noreply.github.com>
2025-07-13 04:16:21 +0600

    Changes to metadata in template

--- src/app/subjects/template/layout.jsx
+++ src/app/subjects/template/layout.jsx
@@ -2,7 +2,6 @@ const subjectName = '{subjectName}';
 const examCode = '{examCode}';
 const syllabusType = '{syllabusType}';
 const refName = subjectName;
-const refexamCode = examCode;
 const refsyllabusType = syllabusType;
 
 export const viewport = {
@@ -10,18 +9,18 @@ export const viewport = {
 }
 
 export const metadata = {
-  title: `${refName} - ${refexamCode}`,
+  title: subjectName,
   description:
-    `Explore a complete collection of learning tools tailored for ${refsyllabusType} ${refName} (${refexamCode}) — including curated revision resources, official past papers, and student-shared notes. Whether you're brushing up on key concepts, preparing for upcoming exams, or looking for simplified explanations from peers, this page brings everything together in one place to help you study smarter and stay ahead.`,
+    `Explore a complete collection of learning tools tailored for ${refsyllabusType} ${refName} — including curated revision resources, official past papers, and student-shared notes. Whether you're brushing up on key concepts, preparing for upcoming exams, or looking for simplified explanations from peers, this page brings everything together in one place to help you study smarter and stay ahead.`,
   icons: {
     icon: "/BlueSolo.svg",
     shortcut: "/BlueSolo.svg",
     apple: "/BlueSolo.svg",
   },
   openGraph: {
-    title: `${refName} - ${refexamCode}`,
+    title: subjectName,
     description:
-      `Explore a complete collection of learning tools tailored for ${refsyllabusType} ${refName} (${refexamCode}) — including curated revision resources, official past papers, and student-shared notes. Whether you're brushing up on key concepts, preparing for upcoming exams, or looking for simplified explanations from peers, this page brings everything together in one place to help you study smarter and stay ahead.`,
+      `Explore a complete collection of learning tools tailored for ${refsyllabusType} ${refName} — including curated revision resources, official past papers, and student-shared notes. Whether you're brushing up on key concepts, preparing for upcoming exams, or looking for simplified explanations from peers, this page brings everything together in one place to help you study smarter and stay ahead.`,
     url: `https://eduvance-website-copy.vercel.app/subjects/${refName}`,
     type: "website",
     images: [
@@ -35,9 +34,9 @@ export const metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: `${refName} - ${refexamCode}`,
+    title: subjectName,
     description:
-      `Explore a complete collection of learning tools tailored for ${refsyllabusType} ${refName} (${refexamCode}) — including curated revision resources, official past papers, and student-shared notes. Whether you're brushing up on key concepts, preparing for upcoming exams, or looking for simplified explanations from peers, this page brings everything together in one place to help you study smarter and stay ahead.`,
+      `Explore a complete collection of learning tools tailored for ${refsyllabusType} ${refName} — including curated revision resources, official past papers, and student-shared notes. Whether you're brushing up on key concepts, preparing for upcoming exams, or looking for simplified explanations from peers, this page brings everything together in one place to help you study smarter and stay ahead.`,
     images: ["https://eduvance-website-copy.vercel.app/SmallLogo.svg"],
   },
 };
