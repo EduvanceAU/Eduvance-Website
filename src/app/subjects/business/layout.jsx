b97d70a58fe92e310de3617936f9ff7f6091ea9a
Rao <103417697+Aby-ss@users.noreply.github.com>
2025-07-13 14:32:43 +0400

    Added all subjects

---
+++ src/app/subjects/business/layout.jsx
@@ -0,0 +1,43 @@
+const subjectName = 'Business';
+const refName = subjectName;
+
+export const viewport = {
+  themeColor: "#4e8cff",
+}
+
+export const metadata = {
+  title: subjectName,
+  description:
+    `Explore a complete collection of learning tools tailored for ${refName} — including curated revision resources, official past papers, and student-shared notes. Whether you're brushing up on key concepts, preparing for upcoming exams, or looking for simplified explanations from peers, this page brings everything together in one place to help you study smarter and stay ahead.`,
+  icons: {
+    icon: "/BlueSolo.svg",
+    shortcut: "/BlueSolo.svg",
+    apple: "/BlueSolo.svg",
+  },
+  openGraph: {
+    title: subjectName,
+    description:
+      `Explore a complete collection of learning tools tailored for ${refName} — including curated revision resources, official past papers, and student-shared notes. Whether you're brushing up on key concepts, preparing for upcoming exams, or looking for simplified explanations from peers, this page brings everything together in one place to help you study smarter and stay ahead.`,
+    url: `https://eduvance-website-copy.vercel.app/subjects/${refName}`,
+    type: "website",
+    images: [
+      {
+        url: "https://eduvance-website-copy.vercel.app/SmallLogo.svg",
+        width: 40,
+        height: 40,
+        alt: "Eduvance Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: subjectName,
+    description:
+      `Explore a complete collection of learning tools tailored for ${refName} — including curated revision resources, official past papers, and student-shared notes. Whether you're brushing up on key concepts, preparing for upcoming exams, or looking for simplified explanations from peers, this page brings everything together in one place to help you study smarter and stay ahead.`,
+    images: ["https://eduvance-website-copy.vercel.app/SmallLogo.svg"],
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (<>{children}</>)
+}
---
+++ src/app/subjects/mathematics/IGCSE/communityNotes/layout.jsx
@@ -0,0 +1,46 @@
+const subjectName = 'Mathematics';
+const examCode = 'MAT';
+const syllabusType = 'IGCSE';
+const refName = subjectName;
+const refsyllabusType = syllabusType;
+
+export const viewport = {
+  themeColor: "#4e8cff",
+}
+
+export const metadata = {
+  title: `${refsyllabusType} ${refName}`,
+  description:
+    `A growing collection of clear, concise notes and study guides for ${refsyllabusType} ${refName}, created entirely by students and teachers who understand what truly helps. Whether you're revising for exams or catching up on topics, these will always be useful`,
+  icons: {
+    icon: "/BlueSolo.svg",
+    shortcut: "/BlueSolo.svg",
+    apple: "/BlueSolo.svg",
+  },
+  openGraph: {
+    title: `${refsyllabusType} ${refName}`,
+    description:
+      `A growing collection of clear, concise notes and study guides for ${refsyllabusType} ${refName}, created entirely by students and teachers who understand what truly helps. Whether you're revising for exams or catching up on topics, these will always be useful`,
+    url: `https://eduvance-website-copy.vercel.app/subjects/${refName}`,
+    type: "website",
+    images: [
+      {
+        url: "https://eduvance-website-copy.vercel.app/SmallLogo.svg",
+        width: 40,
+        height: 40,
+        alt: "Eduvance Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: `${refsyllabusType} ${refName}`,
+    description:
+      `A growing collection of clear, concise notes and study guides for ${refsyllabusType} ${refName}, created entirely by students and teachers who understand what truly helps. Whether you're revising for exams or catching up on topics, these will always be useful`,
+    images: ["https://eduvance-website-copy.vercel.app/SmallLogo.svg"],
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (<>{children}</>)
+}
