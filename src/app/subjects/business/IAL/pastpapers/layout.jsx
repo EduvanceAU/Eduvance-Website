5ec647c9363b920dfc85c24e2563c0fc5766a5c3
Rao <103417697+Aby-ss@users.noreply.github.com>
2025-07-13 14:32:43 +0400

    Added all subjects

---
+++ src/app/subjects/business/IAL/pastpapers/layout.jsx
@@ -0,0 +1,46 @@
+const subjectName = 'Business';
+const examCode = 'BUS';
+const syllabusType = 'IAL';
+const refName = subjectName;
+const refsyllabusType = syllabusType;
+
+export const viewport = {
+  themeColor: "#4e8cff",
+}
+
+export const metadata = {
+  title: `${refsyllabusType} ${refName}`,
+  description:
+    `Access a vast repository of past papers for ${refsyllabusType} ${refName}, organized by year and session for easy browsing. Whether practicing under timed conditions or reviewing marking schemes, this is your one-stop destination for exam preparation — streamlined, reliable, and always up to date`,
+  icons: {
+    icon: "/BlueSolo.svg",
+    shortcut: "/BlueSolo.svg",
+    apple: "/BlueSolo.svg",
+  },
+  openGraph: {
+    title: `${refsyllabusType} ${refName}`,
+    description:
+      `Access a vast repository of past papers for ${refsyllabusType} ${refName}, organized by year and session for easy browsing. Whether practicing under timed conditions or reviewing marking schemes, this is your one-stop destination for exam preparation — streamlined, reliable, and always up to date`,
+    url: `https://eduvance-website-copy.vercel.app/subjects/${refName}`,
+    type: "website",
+    images: [
+      {
+        url: "https://eduvance-website-copy.vercel.app/SmallLogo.svg",
+        width: 40,
+        height: 40,
+        alt: "Eduvance Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: `${refsyllabusType} ${refName}`,
+    description:
+      `Access a vast repository of past papers for ${refsyllabusType} ${refName}, organized by year and session for easy browsing. Whether practicing under timed conditions or reviewing marking schemes, this is your one-stop destination for exam preparation — streamlined, reliable, and always up to date`,
+    images: ["https://eduvance-website-copy.vercel.app/SmallLogo.svg"],
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (<>{children}</>)
+}
---
+++ src/app/subjects/computer/IAL/resources/layout.jsx
@@ -0,0 +1,46 @@
+const subjectName = 'Computer';
+const examCode = 'COM';
+const syllabusType = '';
+const refName = subjectName;
+const refsyllabusType = syllabusType;
+
+export const viewport = {
+  themeColor: "#4e8cff",
+}
+
+export const metadata = {
+  title: `${refsyllabusType} ${refName}`,
+  description:
+    `Explore original resources made by the Eduvance team for ${refsyllabusType} ${refName}. They are designed with precision and purpose, these tools aim to simplify complex concepts and support your revision every step of the way.`,
+  icons: {
+    icon: "/BlueSolo.svg",
+    shortcut: "/BlueSolo.svg",
+    apple: "/BlueSolo.svg",
+  },
+  openGraph: {
+    title: `${refsyllabusType} ${refName}`,
+    description:
+      `Explore original resources made by the Eduvance team for ${refsyllabusType} ${refName}. They are designed with precision and purpose, these tools aim to simplify complex concepts and support your revision every step of the way.`,
+    url: `https://eduvance-website-copy.vercel.app/subjects/${refName}`,
+    type: "website",
+    images: [
+      {
+        url: "https://eduvance-website-copy.vercel.app/SmallLogo.svg",
+        width: 40,
+        height: 40,
+        alt: "Eduvance Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: `${refsyllabusType} ${refName}`,
+    description:
+      `Explore original resources made by the Eduvance team for ${refsyllabusType} ${refName}. They are designed with precision and purpose, these tools aim to simplify complex concepts and support your revision every step of the way.`,
+    images: ["https://eduvance-website-copy.vercel.app/SmallLogo.svg"],
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (<>{children}</>)
+}
