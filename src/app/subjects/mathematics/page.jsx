2a76c01d0640438317c48bde86ba7443dc6d2554
Anonymously Anonymous <99310000+anonymouslyanonymous1@users.noreply.github.com>
2025-07-17 00:44:15 +0600

    Auto select IGCSE tab in subject overview page when arriving from template/IGCSE

--- src/app/subjects/mathematics/page.jsx
+++ src/app/subjects/mathematics/page.jsx
@@ -1,10 +1,18 @@
 "use client";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, use } from 'react';
 import Link from 'next/link';
 
-export default function Subject() {
+export default function Subject({ searchParams }) {
   const subjectName = 'Mathematics';
   const [selected, setSelected] = useState('option1');
+  const params = use(searchParams);
+  const choice = params.choice;
+  const regExp = /option[1-2]/g;
+  useEffect(() => {
+    if (regExp.test(choice)) {
+      setSelected(choice);
+    }
+  }, [choice]);
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   useEffect(() => {
