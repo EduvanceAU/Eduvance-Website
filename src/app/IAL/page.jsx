f0562fd2c54b56c609b459ab3b9eaadacac5554b
Anonymously Anonymous <99310000+anonymouslyanonymous1@users.noreply.github.com>
2025-07-18 22:43:12 +0600

    Responsiveness for the list

--- src/app/IAL/page.jsx
+++ src/app/IAL/page.jsx
@@ -26,10 +26,10 @@ export default function IALSubjectsPage() {
         <h1 className="text-4xl font-semibold leading-[40px] tracking-tighter" style={{ fontFamily: 'Poppins, sans-serif' }}>
           Our <span className="bg-[#0C58E4] text-white py-[1.5] px-1 -rotate-1 inline-block">IAL</span> Subject List
         </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full">
           {subjects.map((subject) => (
             <a key={subject.name} href={`/subjects/${subject.toLowerCase()}?choice=option1`}>
-              <div className="cursor-pointer flex items-center justify-between px-6 w-[350px] py-4 bg-[#BAD1FD] rounded-[12px] group hover:bg-[#A8C6FF] transition-all duration-200 border-[#153064] border-1">
+              <div className="cursor-pointer flex items-center justify-between px-6 w-full py-4 bg-[#BAD1FD] rounded-[12px] group hover:bg-[#A8C6FF] transition-all duration-200 border-[#153064] border-1">
                 <p
                   className="text-xl font-[550] text-[#153064]"
                   style={{ fontFamily: "Poppins, sans-serif" }}
