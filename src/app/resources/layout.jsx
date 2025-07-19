ba3232be491b097776a2f815db37ba63aa2c1931
Rao <103417697+Aby-ss@users.noreply.github.com>
2025-07-19 16:15:18 +0400

    Updated URL

--- src/app/resources/layout.jsx
+++ src/app/resources/layout.jsx
@@ -15,11 +15,11 @@ export const metadata = {
     title: "Resources",
     description:
       "Browse a diverse library of shared resources — including notes, study guides, slides, and question banks — contributed by both students and teachers. These materials are here to support your learning and foster a more open, collaborative approach to education!",
-    url: `https://eduvance-website-copy.vercel.app/resources`,
+    url: `https://www.eduvance-org.vercel.app/resources`,
     type: "website",
     images: [
       {
-        url: "https://eduvance-website-copy.vercel.app/tempbg.png",
+        url: "https://www.eduvance-org.vercel.app/tempbg.png",
         width: 1200,
         height: 630,
         alt: "Eduvance Image",
@@ -31,7 +31,7 @@ export const metadata = {
     title: "Resources",
     description:
       "Browse a diverse library of shared resources — including notes, study guides, slides, and question banks — contributed by both students and teachers. These materials are here to support your learning and foster a more open, collaborative approach to education!",
-    images: ["https://eduvance-website-copy.vercel.app/tempbg.png"],
+    images: ["https://www.eduvance-org.vercel.app/tempbg.png"],
   },
 };
 
