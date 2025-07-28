82d3bce194ac5ddd359fb666fa5de0ebf347426d
Anonymously Anonymous <99310000+anonymouslyanonymous1@users.noreply.github.com>
2025-07-28 14:06:02 +0600

    Footer always at bottom + Responsive to sidebar

--- src/components/smallFoot.jsx
+++ src/components/smallFoot.jsx
@@ -2,10 +2,48 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import BiggerLogo from '@/assets/png/BiggerLogo.png'
-
+import { useEffect, useState } from 'react';
 export default function SmallFoot() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  useEffect(() => {
+    const target = document.querySelector('.sidebarWheel');
+    if (!target) return;
+
+    const observer = new MutationObserver((mutationsList) => {
+      for (const mutation of mutationsList) {
+        if (
+          mutation.type === 'attributes' &&
+          mutation.attributeName === 'data-sidebar'
+        ) {
+          const value = target.getAttribute('data-sidebar');
+          if(value === "open"){
+            setSidebarOpen(true)
+          }
+          else{
+            setSidebarOpen(false)
+          }
+        }
+      }
+    });
+
+    observer.observe(target, { attributes: true, attributeFilter: ['data-sidebar'] });
+
+    return () => observer.disconnect();
+  }, []);
+  const[ratio, setRatio] = useState(1)
+  useEffect(() => {
+    const updateRatio = () => {
+      setRatio(window.devicePixelRatio); 
+    };
+    updateRatio();
+    window.addEventListener("resize", updateRatio);
+    return () => {
+      window.removeEventListener("resize", updateRatio);
+    };
+  }, []);
   return (
-    <footer className="w-full bg-white pt-10 pb-0 border-t border-[#e5e7eb]">
+    <footer className={`bg-white pt-10 pb-0 border-t border-[#e5e7eb] ${sidebarOpen && ratio > 0.85 ? 'sm:ml-70 w-fit' : 'w-full'} ${ratio < 0.85 ? "absolute bottom-0" : ""} transition-all duration-300`}>
       <div className="container mx-auto px-4 relative">
         <div className="rounded-2xl p-6 max-w-[1850px] mx-auto flex flex-col lg:flex-row sm:gap-8">
           {/* Left Column */}
@@ -17,9 +55,9 @@ export default function SmallFoot() {
           </div>
 
           {/* Right Column - 3 Inner Columns */}
-          <div className="w-full lg:w-2/3 grid grid-cols-2 sm:grid-cols-3 gap-1 mt-[20px]">
+          <div className="w-full lg:w-2/3 grid grid-cols-1 sm:grid-cols-3 gap-1 mt-[20px]">
             {/* Column 1 */}
-            <div className="flex flex-col max-sm:items-center">
+            <div className="flex flex-col">
               <h3 className="text-black tracking-[-0.6px] text-lg font-[550] mb-2" style={{ fontFamily: 'Poppins, sans-serif' }}>Navigation</h3>
               <Link href="/" className="text-[#757575] tracking-[-0.5px] font-medium hover:text-slate-950" style={{ fontFamily: 'Poppins, sans-serif' }}>Home</Link>
               <Link href="/resources" className="text-[#757575] tracking-[-0.5px] font-medium hover:text-slate-950" style={{ fontFamily: 'Poppins, sans-serif' }}>Resources</Link>
@@ -30,7 +68,7 @@ export default function SmallFoot() {
             </div>
 
             {/* Column 2 */}
-            <div className="flex flex-col max-sm:items-center">
+            <div className="flex flex-col">
               <h3 className="text-black tracking-[-0.6px] text-lg font-[550] mb-2" style={{ fontFamily: 'Poppins, sans-serif' }}>Legal Info</h3>
               <a href="/guidelines" className="text-[#757575] tracking-[-0.5px] font-medium hover:text-slate-950" style={{ fontFamily: 'Poppins, sans-serif' }}>Community Guidelines</a>
               <a href="/terms" className="text-[#757575] tracking-[-0.5px] font-medium hover:text-slate-950" style={{ fontFamily: 'Poppins, sans-serif' }}>Terms of Services</a>
@@ -38,7 +76,7 @@ export default function SmallFoot() {
             </div>
 
             {/* Column 3 */}
-            <div className="flex flex-col max-sm:col-span-2 max-sm:items-center max-sm:mt-3">
+            <div className="flex flex-col">
               <p className="text-black tracking-[-0.6px] text-lg font-[550] mb-2" style={{ fontFamily: 'Poppins, sans-serif' }}>Join the Community</p>
               <div className='flex gap-2 items-center justify-start mb-2'>
                 <a aria-label='discord' href="https://discord.gg/eduvance-community-983670206889099264" className='w-6'><svg className="hover:fill-[#0c60fb] cursor-pointer" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24"><title>Discord</title><path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z"/></svg></a>
@@ -52,7 +90,7 @@ export default function SmallFoot() {
         </div>
       </div>
       {/* Blue strip at the bottom */}
-      <div className="w-full bg-[#357BFD] text-white text-center py-3 mt-8 text-sm font-medium flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2 px-4" style={{ fontFamily: 'Poppins, sans-serif' }}>
+      <div className={`w-full bg-[#357BFD] ${sidebarOpen && ratio > 0.85 ? 'sm:pl-10 sm:text-xs' : 'text-sm'} text-white text-center py-3 mt-2 font-medium flex flex-col sm:flex-row sm:justify-evenly sm:items-center gap-2 px-4`} style={{ fontFamily: 'Poppins, sans-serif' }}>
         <span className="block text-left font-grand-small">
           Eduvance.au is a student-led initiative non-commercial organisation and not yet a registered business entity.
         </span>
