d3c5e17fbc4f3547c14502a2aff9dcd4e63f4091
Anonymously Anonymous <99310000+anonymouslyanonymous1@users.noreply.github.com>
2025-08-07 22:30:00 +0600

    Meta Data for few pages

---
+++ src/app/privacy/layout.jsx
@@ -0,0 +1,41 @@
+export const viewport = {
+  themeColor: "#4e8cff",
+}
+
+export const metadata = {
+  title: `Privacy Policy`,
+  description:
+    `Learn how we collect, use, and protect your personal information. Our Privacy Policy outlines your data rights, the security measures we take, and how we ensure transparency. Review this policy to understand how your privacy is handled when using our services or interacting with our platform.`,
+  icons: {
+    icon: "/BlueSolo.svg",
+    shortcut: "/BlueSolo.svg",
+    apple: "/BlueSolo.svg",
+  },
+  openGraph: {
+    title: `Privacy Policy`,
+    description:
+      `Learn how we collect, use, and protect your personal information. Our Privacy Policy outlines your data rights, the security measures we take, and how we ensure transparency. Review this policy to understand how your privacy is handled when using our services or interacting with our platform.`,
+    url: `https://www.eduvance-org.vercel.app/privacy`,
+    type: "website",
+    images: [
+      {
+        url: "https://www.eduvance-org.vercel.app/tempbg.png",
+        width: 1200,
+        height: 630,
+        alt: "Eduvance Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: `Privacy Policy`,
+    description:
+      `Learn how we collect, use, and protect your personal information. Our Privacy Policy outlines your data rights, the security measures we take, and how we ensure transparency. Review this policy to understand how your privacy is handled when using our services or interacting with our platform.`,
+    images: ["https://www.eduvance-org.vercel.app/tempbg.png"],
+  },
+};
+
+import {Home} from '@/components/homenav'
+export default function RootLayout({ children }) {
+  return (<><Home showExtra/> {children}</>)
+}
