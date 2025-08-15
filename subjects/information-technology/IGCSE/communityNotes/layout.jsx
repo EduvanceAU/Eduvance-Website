2ad85f6db59c81434e699233fe3747b407ad0248
Anonymously Anonymous <99310000+anonymouslyanonymous1@users.noreply.github.com>
2025-08-15 21:22:15 +0600

    Specific Resource Type (tag) Selector

---
+++ subjects/information-technology/IGCSE/communityNotes/layout.jsx
@@ -0,0 +1,46 @@
+const subjectName = 'Information Technology';
+const examCode = '4IT1';
+const syllabusType = 'IGCSE';
+const refName = subjectName;
+const refsyllabusType = syllabusType;
+
+export const viewport = {
+  themeColor: "#4e8cff",
+}
+
+export const metadata = {
+  title: `${refsyllabusType} ${refName}`,
+  description:
+    `Discover clear, concise notes and study guides for ${refsyllabusType} ${refName}, created by students and teachers who get what works. Ideal for exam preparation or topic review, these resources are made to support your learning every step of the way!`,
+  icons: {
+    icon: "/BlueSolo.svg",
+    shortcut: "/BlueSolo.svg",
+    apple: "/BlueSolo.svg",
+  },
+  openGraph: {
+    title: `${refsyllabusType} ${refName}`,
+    description:
+      `Discover clear, concise notes and study guides for ${refsyllabusType} ${refName}, created by students and teachers who get what works. Ideal for exam preparation or topic review, these resources are made to support your learning every step of the way!`,
+    url: `https://eduvance-org.vercel.app/subjects/${refName}`,
+    type: "website",
+    images: [
+      {
+        url: "https://eduvance-org.vercel.app/tempbg.png",
+        width: 1200,
+        height: 630,
+        alt: "Eduvance Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: `${refsyllabusType} ${refName}`,
+    description:
+      `Discover clear, concise notes and study guides for ${refsyllabusType} ${refName}, created by students and teachers who get what works. Ideal for exam preparation or topic review, these resources are made to support your learning every step of the way!`,
+    images: ["https://eduvance-org.vercel.app/tempbg.png"],
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (<>{children}</>)
+}
